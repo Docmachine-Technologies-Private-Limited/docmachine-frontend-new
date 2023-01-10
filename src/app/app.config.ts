7dcb53366c83491c1e8ae57e22e3ae8653d0f030
abhishekkumar1996 <122076647+abhishekkumar1996@users.noreply.github.com>
2023-01-10 10:23:55 +0530

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -19,18 +19,18 @@ export class AppConfig {
     }
 
     public getConstantsAtServer() {
-        this.apiUrl = `http://localhost:8080/v1`;
-        // this.apiUrl = `https://stagingapi.bharathexim.com/v1`;
+        //this.apiUrl = `http://localhost:8080/v1`;
+        this.apiUrl = `https://stagingapi.bharathexim.com/v1`;
         // this.apiUrl = `http://localhost:8080/v1`;
         // this.apiUrl = `https://api.bharathexim.com/v1`;
     }
 
 
     public getConstants() {
-        // this.apiUrl = `https://stagingapi.bharathexim.com/v1`;
+        this.apiUrl = `https://stagingapi.bharathexim.com/v1`;
         // this.apiUrl = `https://devapi.bharathexim.com/v1`;
         // this.apiUrl = `https://api.bharathexim.com/v1`;
-        this.apiUrl = `https://api.bharathexim.com/v1`;
+//         this.apiUrl = `https://api.bharathexim.com/v1`;
 
         // if (window.location.hostname === 'localhost') {
             // this.apiUrl = `http://localhost:8080/v1`;
