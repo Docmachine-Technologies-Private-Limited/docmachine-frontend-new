385059f0a5daae6d509058db41ce5a6d39a6b3c4
iTBharathexim <125201575+iTBharathexim@users.noreply.github.com>
2023-02-17 19:40:32 +0530

    'some2'

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -20,7 +20,7 @@ export class AppConfig {
 
     public getConstantsAtServer() {
         // this.apiUrl = `https://stagingapi.bharathexim.com/v1`;
-        this.apiUrl = `https://testapi.bharathexim.com/v1`;
+        this.apiUrl = `https://demoapi.bharathexim.com/v1`;
         // this.apiUrl = `http://localhost:8080/v1`;
         // this.apiUrl = `https://devappapi.bharathexim.com/v1`;
     }
