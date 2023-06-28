0da205e2cddc48e66d8981b5939cc78c1590872a
iTBharathexim <125201575+iTBharathexim@users.noreply.github.com>
2023-06-28 15:24:35 +0530

    454ewtewt345tgfgfdgfd

--- src/environments/environment.live.ts
+++ src/environments/environment.live.ts
@@ -1,8 +1,8 @@
 export const AppConfig = {
   production: false,
   environment: 'Live',
-  BASE_URL: 'https://dockerapi.bharathexim.com/v1',
-  SOCKET_ENDPONT: 'https://dockerapi.bharathexim.com',
+  BASE_URL: 'https://liveapi.bharathexim.com/v1',
+  SOCKET_ENDPONT: 'https://liveapi.bharathexim.com',
   ROLE_URL:'https://role.bharathexim.com',
   FRONT_END_URL:'https://www.bharathexim.com/',
   S3_BUCKET_URL:'https://devassets.bharathexim.com/'
