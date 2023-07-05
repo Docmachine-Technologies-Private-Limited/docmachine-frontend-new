926d65545dc63eb7027d333d4d16a0c784efa811
iTBharathexim <125201575+iTBharathexim@users.noreply.github.com>
2023-07-06 02:59:07 +0530

    asdaseqwdasdsads

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,8 +1,8 @@
 export const AppConfig = {
   production: false,
   environment: 'DEV',
-  BASE_URL: 'https://dockerapi.bharathexim.com/v1',
-  SOCKET_ENDPONT: 'https://dockerapi.bharathexim.com',
+  BASE_URL: 'https://devapi.bharathexim.com/v1',
+  SOCKET_ENDPONT: 'https://devapi.bharathexim.com',
   ROLE_URL:'https://role.bharathexim.com',
   FRONT_END_URL:'https://devapp.bharathexim.com/',
   S3_BUCKET_URL:'https://devassets.bharathexim.com/'
