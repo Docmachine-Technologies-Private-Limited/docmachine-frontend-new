6dac814c579b5223a3a2b85e51c4465c6d4cc6d6
iTBharathexim <125201575+iTBharathexim@users.noreply.github.com>
2024-02-10 10:51:41 +0530

    'added'

--- src/app/RoleBased/LiveTradeApp/LiveTradeApp.component.ts
+++ src/app/RoleBased/LiveTradeApp/LiveTradeApp.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 import { UserService } from '../../service/user.service';
 import { ToastrService } from 'ngx-toastr';
-import { MatCheckboxChange } from '@angular/material/checkbox';
 
 @Component({
   selector: 'LiveTradeApp',
@@ -10,36 +9,39 @@ import { MatCheckboxChange } from '@angular/material/checkbox';
   styleUrls: ['./LiveTradeApp.component.scss']
 })
 export class LiveTradeAppComponent implements OnInit {
-  userForm:any = new FormGroup({
+  userForm: any = new FormGroup({
     emailId: new FormControl(),
     password: new FormControl(),
     firstName: new FormControl(),
     last_name: new FormControl(),
     Permission: new FormControl()
   });
-  interests:any=[{name:'RBI',value:'RBI'}];
+  interests: any = [
+    { name: 'RBI', value: 'RBI' },
+    { name: 'BenchMarks', value: 'BenchMarks' }
+  ];
   constructor(public userService: UserService,
     private toastr: ToastrService,) { }
 
   ngOnInit(): void {
   }
-  
+
   onFormSubmit() {
-    this.userForm.value.role="TradeApp"
+    this.userForm.value.role = "TradeApp"
     this.userService.DeltaTradeAppAddUser(this.userForm.value).subscribe((res: any) => {
       console.log(res, 'hfhffgffg')
       this.toastr.success('Successfully create Trade App account...');
       this.toastr.success('Also check registrated email id..')
-    },(err)=>{
-       this.toastr.error(err?.error?.text);
-       console.log(err,'sdfsdhfsdklfhdsfksdfsdfds')
+    }, (err) => {
+      this.toastr.error(err?.error?.text);
+      console.log(err, 'sdfsdhfsdklfhdsfksdfsdfds')
     })
     console.log(this.userForm.value, 'sdsgjdfgsdfdf')
   }
-  
-  RoleType:any=''
-  ChangesRoleType(role:any){
-    this.RoleType=role?.value;
+
+  RoleType: any = ''
+  ChangesRoleType(role: any) {
+    this.RoleType = role?.value;
   }
-  
+
 }
